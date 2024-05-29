cafba21ab8ff06598432d9f028cd26fe3c922fdd
foxgem <jianhgreat@hotmail.com>
2024-05-29 15:31:33 +1000

    add bot app skeleton

---
+++ apps/bot/api/index.ts
@@ -0,0 +1,15 @@
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+
+export const config = {
+  runtime: "edge",
+};
+
+const app = new Hono().basePath("/api");
+
+app.all("/", (c) => {
+  console.log(JSON.stringify(c.req));
+  return c.json({ message: "Hello Hono!" });
+});
+
+export default handle(app);
