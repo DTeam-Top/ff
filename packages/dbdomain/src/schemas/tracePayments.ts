4623fbd3395f569490644960bd1ace2c9d5c2382
胡伟红 <weihong0427@163.com>
2024-05-30 20:27:14 +0800

    fix: 🐛 typo in comments

--- packages/dbdomain/src/schemas/tracePayments.ts
+++ packages/dbdomain/src/schemas/tracePayments.ts
@@ -7,8 +7,8 @@ export const tracePayments = sqliteTable(
     trace: text("trace").notNull(), // trace id
     amount: integer("amount").notNull(), // payment amount, eth
     paymentTx: text("payment_tx").notNull(), // payment tx hash
-    paymentTs: integer("payment_ts", { mode: "timestamp" }), // payment timestamp, -1 for not paid, 1 for paid
-    commissionPaid: integer("commission_paid"), // payment timestamp, -1 for not paid, 1 for paid
+    paymentTs: integer("payment_ts", { mode: "timestamp" }), // payment timestamp
+    commissionPaid: integer("commission_paid"), //  -1 for not paid, 1 for paid
   },
   (tracePayments) => ({
     idxTrace: index("idx_tp_trace").on(tracePayments.trace),
