0c914c56cfd3414887f4fbadbcdaf3078d1fd97b
foxgem <jianhgreat@hotmail.com>
2024-06-10 17:13:23 +1000

    refactor Mocks

--- contracts/test/Mocks.sol
+++ contracts/test/Mocks.sol
@@ -5,25 +5,19 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
-contract MyERC20 is ERC20 {
-  constructor() ERC20("MyERC20", "M20") {}
-
+contract MyERC20 is ERC20("MyERC20", "M20") {
   function mint(uint256 amount, address receiver) external {
     _mint(receiver, amount);
   }
 }
 
-contract MyERC721 is ERC721 {
-  constructor() ERC721("MyERC721", "M721") {}
-
+contract MyERC721 is ERC721("MyERC721", "M721") {
   function mint(uint256 id, address receiver) external {
     _mint(receiver, id);
   }
 }
 
-contract MyERC1155 is ERC1155 {
-  constructor() ERC1155("") {}
-
+contract MyERC1155 is ERC1155("") {
   function mint(uint256 id, uint256 amount, address receiver) external {
     _mint(receiver, id, amount, "");
   }
