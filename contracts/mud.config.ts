81122d965256a11bedb5827cd36ad501dbeba7e2
foxgem <jianhgreat@hotmail.com>
2024-06-11 14:33:20 +1000

    update mud config

--- contracts/mud.config.ts
+++ contracts/mud.config.ts
@@ -5,16 +5,16 @@ export default defineWorld({
     BlacklistSystem: {
       openAccess: false,
       // FlowsDvp Address: local test
-      // accessList: ["0x5991A2dF15A8F6A256D3Ec51E99254Cd3fb576A9"],
+      accessList: ["0x5991A2dF15A8F6A256D3Ec51E99254Cd3fb576A9"],
       // FlowsDvp Address: base sepolia
-      accessList: ["0xFcAc032068C867373d0BA48aB0fa83142D557069"],
+      // accessList: ["0xFcAc032068C867373d0BA48aB0fa83142D557069"],
     },
     CommissionSystem: {
       openAccess: false,
       // FlowsDvp Address: local test
-      // accessList: ["0x5991A2dF15A8F6A256D3Ec51E99254Cd3fb576A9"],
+      accessList: ["0x5991A2dF15A8F6A256D3Ec51E99254Cd3fb576A9"],
       // FlowsDvp Address: base sepolia
-      accessList: ["0xFcAc032068C867373d0BA48aB0fa83142D557069"],
+      // accessList: ["0xFcAc032068C867373d0BA48aB0fa83142D557069"],
     },
   },
   tables: {
